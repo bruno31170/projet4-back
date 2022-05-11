86b1a1af409aefb6252eb7f476729cdeac12afcc
bruno31170 <bru.boulet@gmail.com>
2022-05-11 15:17:00 +0200

    final commit n2

--- src/main/java/fr/isika/al12/springjwt/controllers/AuthController.java
+++ src/main/java/fr/isika/al12/springjwt/controllers/AuthController.java
@@ -100,22 +100,7 @@ public class AuthController {
 			Role userRole = roleRepository.findByName(ERole.ROLE_USER)
 					.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
 			roles.add(userRole);
-		} else {
-			strRoles.forEach(role -> {
-				switch (role) {
-				case "admin":
-					Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
-							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-					roles.add(adminRole);
-
-					break;
-				default:
-					Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-					roles.add(userRole);
-				}
-			});
-		}
+		} 
 
 		user.setRoles(roles);
 		userRepository.save(user);
